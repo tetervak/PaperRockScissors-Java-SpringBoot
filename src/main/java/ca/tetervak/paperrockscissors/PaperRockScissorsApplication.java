6e698095b02d753e750670b5af67b46a9df7c431
Alexander Tetervak <tetervak@sheridancollege.ca>
2023-01-24 19:32:03 -0500

    initial commit

---
+++ src/main/java/ca/tetervak/paperrockscissors/PaperRockScissorsApplication.java
@@ -0,0 +1,13 @@
+package ca.tetervak.paperrockscissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperRockScissorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperRockScissorsApplication.class, args);
+    }
+
+}
