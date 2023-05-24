c37a3aab543f2856caf1dacb7cb65c4caae85862
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-23 20:27:42 -0400

    simplified the output logic in
    (cherry picked from commit 2a1dc588c816beeb8561fd3fe910042f0ae7f4fa)

--- src/main/java/ca/tetervak/paperrockscissors/model/GameResult.java
+++ src/main/java/ca/tetervak/paperrockscissors/model/GameResult.java
@@ -3,5 +3,10 @@ package ca.tetervak.paperrockscissors.model;
 public enum GameResult {
     PLAYER_ONE_WINS,
     PLAYER_TWO_WINS,
-    REPLAY
+    REPLAY;
+
+    @Override
+    public String toString() {
+        return super.toString().toLowerCase();
+    }
 }
