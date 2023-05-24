a016f91f266dd15d5ff96c51adf05a36ab5eddbe
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-23 20:03:52 -0400

    removed the formatter for simplicity

--- src/main/java/ca/tetervak/paperrockscissors/model/Choice.java
+++ src/main/java/ca/tetervak/paperrockscissors/model/Choice.java
@@ -2,4 +2,9 @@ package ca.tetervak.paperrockscissors.model;
 
 public enum Choice {
     PAPER, ROCK, SCISSORS;
+
+    @Override
+    public String toString() {
+        return super.toString().toLowerCase();
+    }
 }
