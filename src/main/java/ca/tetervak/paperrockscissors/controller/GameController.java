15638e4ca086bc83e3d184c3406a400495be54e6
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-29 14:00:43 -0500

    removed unused import

--- src/main/java/ca/tetervak/paperrockscissors/controller/GameController.java
+++ src/main/java/ca/tetervak/paperrockscissors/controller/GameController.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
